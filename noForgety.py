af1b8e61c2e5c647cf0c6cbb0e614b4efdfc89d4
1201313_AnaCosta <1201313@isep.ipp.pt>
2025-09-06 11:41:16 +0100

    Checking today birthdays and sending message.

--- noForgety.py
+++ noForgety.py
@@ -7,31 +7,23 @@ from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 intents = discord.Intents.default()
-
 bot = commands.Bot(command_prefix=None, intents=intents)
+file = 'bdays.json'
 
 @bot.event
 async def on_ready():
-    print('Bot connected!')
-    try:
-        synced = await bot.tree.sync()
-        print(f'Synced {len(synced)} commands!')
-    except Exception as e:
-        print(e)
+    await bot.tree.sync()
 
 @bot.tree.command(name="hello", description="Replies with hello!")
 async def test_command(interaction: discord.Interaction):
     await interaction.response.send_message("Hello!!")
 
 @bot.tree.command(name="add_bday", description="Add a birthday! Format: MM-DD")
-async def add_bday(interaction: discord.Interaction, name: str, date: str):
-    try:
-        datetime.strptime(f"2000-{date}", "%Y-%m-%d")
-    except ValueError:
+async def add_bday(interaction: discord.Interaction, name: str, date: str):    
+    if not validate_date(date):
         await interaction.response.send_message("Invalid date format. Please use MM-DD.")
-        return   
+        return
     
     bdays = load_birthdays()
 
@@ -43,12 +35,28 @@ async def add_bday(interaction: discord.Interaction, name: str, date: str):
 
     await interaction.response.send_message(f"{name}'s birthday was added!")
 
+@bot.tree.command(name="check_bdays", description="Check today birthdays!")
+async def check_today_birthdays(interaction: discord.Interaction):
+    today = datetime.now().strftime("%m-%d")
+    bdays = load_birthdays()
+
+    for bday in bdays["birthdays"]:
+        if bday["date"] == today:
+            await interaction.response.send_message(f"Today is {bday['name']}'s birthday!!")
+
+def validate_date(date: str) -> bool:
+    try:
+        datetime.strptime(f"2000-{date}", "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
 def load_birthdays():
-    with open('bdays.json', 'r') as f:
+    with open(file, 'r') as f:
         return json.load(f)
 
 def save_birthdays(bdays):
-    with open('bdays.json', 'w') as f:
+    with open(file, 'w') as f:
         json.dump(bdays, f, indent=4)
 
 bot.run(TOKEN)
